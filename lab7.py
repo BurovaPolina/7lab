2d849f0c17a15d02af50cbf0605e1cae610b5f4d
BurovaPolina <128833381+BurovaPolina@users.noreply.github.com>
2023-11-27 17:04:34 +0300

    Update lab7.py

--- lab7.py
+++ lab7.py
@@ -1,6 +1,58 @@
-from weekdiet import WeekiDiet
-
 days = 7
+
+
+class Ice_cream:
+   def __init__(self, name, quality, energy):
+      self.name = name
+      self.quality = quality
+      self.energy = energy
+
+   name = ''
+   quality = ''
+   energy = 0
+
+   def __str__(self):
+      return 'name:' + self.name + ' quality:' + self.quality + ' energy:' + str(self.energy)
+
+class WeekiDiet:
+   full_list = []
+
+   def init(self):
+      print("init")
+      self.full_list.append(Ice_cream("Шоколад", "качественное", 100))
+      self.full_list.append(Ice_cream("Банан", "некачественное", 150))
+      self.full_list.append(Ice_cream("Клубника", "некачественное", 120))
+      self.full_list.append(Ice_cream("Апельсин", "качественное", 80))
+      self.full_list.append(Ice_cream("Ваниль", "некачественное", 110))
+      self.full_list.append(Ice_cream("Лесные ягоды", "качественное", 90))
+      self.full_list.append(Ice_cream("Облепиха", "некачественное", 130))
+      self.full_list.append(Ice_cream("Смородина", "качественное", 70))
+      self.full_list.append(Ice_cream("Крем-брюле", "некачественное", 140))
+      self.full_list.append(Ice_cream("Сыр", "качественное", 60))
+
+   # вернуть полный список для разных вариантов
+   def get_full_list(self):
+      return self.full_list
+
+   # получить количество каллорий для списка combination
+   def calculate_total_calories(self, combination):
+      return sum(ice_cream.energy for ice_cream in combination)
+
+   # выборка для всех вариантов от текущего day
+   def default_selection(self, ice_creams, day, days):
+      for sub_day in range(day, days):
+         # проходим для каждого по выборке от 0 до 2
+         total_calories = 0
+         for selection in range(3):
+            print('День:', sub_day + 1)
+            if len(ice_creams[:selection]) == 0:
+               print('список пуст, количесиво калорий 0')
+            else:
+               total_calories += self.calculate_total_calories(ice_creams[selection:])
+               print('количество калорий в день', sub_day, ' :', self.calculate_total_calories(ice_creams[selection:]))
+               print(*ice_creams[:selection], "\n")
+            del ice_creams[:selection]
+      return total_calories
 
 def main():
    wd = WeekiDiet()
